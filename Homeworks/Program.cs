581a2835aa4550ce38feee08c80470eb7f9e0bf1
Olesia <test@test.test>
2020-12-16 19:46:17 +0400

    * .gitignore: first
    * Homeworks.sln:
    * Program.cs:
    * Homeworks.csproj:
    * AssemblyInfo.cs:

---
+++ Homeworks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Homeworks
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
